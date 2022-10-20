417fde5122faf9ed629f1426378b5ecf463206ee
ZacharyDonovan88 <zachary.donovan88@outlook.com>
2022-10-20 18:52:35 +1030

    update JS

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -24,6 +24,7 @@ function ytVideos(results) {
     }
 };
 
+/*
 // https://www.
 // document.getElementById('option').addEventListener('change', function() {
 //     console.log('You selected: ', this.value);
@@ -46,3 +47,153 @@ function getdatafromdog() {
 }
 
 // getdatafromdog();
+*/
+
+
+
+var selectList = document.getElementById("breedSelection").option;
+console.log("breedSelection");
+
+var breedSelectionList = [
+    'Labrador',
+    "Golden Retriever",
+    "French Bulldog",
+    "Beagle",
+    "German Shepherd",
+    "Poodle",
+    "Border Collie",
+    "Boxer",
+    "Cavalier King Charles Spaniel",
+    "Schnauzer",
+    "Pug",
+    ]
+
+
+var optionList = (breedSelectionList + selectList);
+
+
+var dogApi = 'https://api.thedogapi.com/v1/breeds/';
+
+
+
+
+
+function getdatafromdog(dogTypeAsString) {
+    switch (dogTypeAsString) {
+        case 'Labrador':
+            var chosenDogId = '149';
+            break;
+        case 'Golden Retriever':
+            var chosenDogId = '121';
+            break;
+        case 'French Bulldog':
+            var chosenDogId = '113';
+            break;
+        case 'Beagle':
+            var chosenDogId = '31';
+            break;
+        case 'German Shepherd':
+            var chosenDogId = '115';
+            break;
+        case 'Poodle':
+            var chosenDogId = '196';
+            break;
+        case 'Border Collie':
+            var chosenDogId = '50';
+            break;            
+        case 'Boxer':
+            var chosenDogId = '55';
+            break; 
+        case 'Cavalier King Charles Spaniel':
+            var chosenDogId = '71';
+            break; 
+        case 'Schnauzer':
+            var chosenDogId = '239';
+            break; 
+        case 'Pug':
+            var chosenDogId = '201';
+            break;       
+    }
+
+    fetch(dogApi + chosenDogId)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        var dogNameEl = document.getElementById('breed_name');
+        dogNameEl.innerHTML = data.name;    
+    })
+
+
+fetch(dogApi + chosenDogId)
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    console.log(data);
+    var dogHeightEl = document.getElementById('height');
+    dogHeightEl.innerHTML = data.height;    
+})
+
+
+
+fetch(dogApi + chosenDogId)
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    console.log(data);
+    var dogYearsEl = document.getElementById('life_span');
+    dogYearsEl.innerHTML = data.life_span;    
+})
+
+fetch(dogApi + chosenDogId)
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    console.log(data);
+    var dogLapDogEl = document.getElementById('bred_for');
+    dogLapDogEl.innerHTML = data.bred_for;    
+})
+
+fetch(dogApi + chosenDogId)
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    console.log(data);
+    var dogTempEl = document.getElementById('temperament');
+    dogTempEl.innerHTML = data.temperament;    
+})
+
+fetch(dogApi + chosenDogId)
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    console.log(data);
+    var dogWeightEl = document.getElementById('weight');
+    dogWeightEl.innerHTML = data.weight;    
+})
+
+}
+
+
+
+/*
+
+function getdatafromyt(option) {
+    parentDiv.innerHTML = '';
+    fetch(ytApi + option)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data.items);
+        ytVideos(data.items);
+    })
+};
+*/
+
